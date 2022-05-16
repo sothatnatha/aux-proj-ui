8e62f3519d1eb2f32f2d7c97f206026aca2f8f76
thatna2211 <Sothatna3121@gmail.com>
2022-05-16 17:00:32 +0700

    new build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: 'homepage', component: HomePageComponent},
-  { path: '**', redirectTo: 'homepage', pathMatch: 'full'}
+  { path: 'homepage', component: AppComponent },
+  { path: '**', redirectTo: 'homepage', pathMatch: 'full' },
 ];
 
 @NgModule({
